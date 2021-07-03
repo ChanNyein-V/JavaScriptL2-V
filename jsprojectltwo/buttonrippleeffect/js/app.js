d7a15b41e5cf45a4ab847233e4a9e3991e782b29
ChanNyein-V <ayechannyein.vishu@gmail.com>
2021-07-03 18:20:58 +0630

    3July

---
+++ jsprojectltwo/buttonrippleeffect/js/app.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.btnripple');
+// console.log(buttons);
+
+buttons.forEach(function(button){
+    // console.log(button);
+
+    button.addEventListener('click',function(){
+
+    });
+});
