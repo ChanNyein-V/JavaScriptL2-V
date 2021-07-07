57625c39bc2171f1807217bfcd3b3e38f074580e
ChanNyein-V <ayechannyein.vishu@gmail.com>
2021-07-08 01:35:59 +0630

    4July

--- jsprojectltwo/buttonrippleeffect/js/app.js
+++ jsprojectltwo/buttonrippleeffect/js/app.js
@@ -5,6 +5,9 @@ buttons.forEach(function(button){
     // console.log(button);
 
     button.addEventListener('click',function(){
+        // console.log('hay');
 
+        /* button click ခေါက်တဲ့နေရာကို center ယူပြီ circle ကကြီးလာရမှာ
+        pointer ရဲ့ x,y ကိုသိဖို့လို့တယ် click ခေါက်တဲ့အချိန်ကျရင်  */
     });
 });
