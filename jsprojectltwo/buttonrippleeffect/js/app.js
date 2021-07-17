1493a62b150f658c1c42848fbdf22c9b3461a1de
ChanNyein-V <ayechannyein.vishu@gmail.com>
2021-07-17 11:25:22 +0630

    3July2021

--- jsprojectltwo/buttonrippleeffect/js/app.js
+++ jsprojectltwo/buttonrippleeffect/js/app.js
@@ -1,13 +1,47 @@
 const buttons = document.querySelectorAll('.btnripple');
 // console.log(buttons);
 
-buttons.forEach(function(button){
+buttons.forEach(function (button){
     // console.log(button);
-
-    button.addEventListener('click',function(){
+    
+    button.addEventListener('click',function (e) {
         // console.log('hay');
+        // console.log(e);
 
-        /* button click ခေါက်တဲ့နေရာကို center ယူပြီ circle ကကြီးလာရမှာ
-        pointer ရဲ့ x,y ကိုသိဖို့လို့တယ် click ခေါက်တဲ့အချိန်ကျရင်  */
+        const cx = e.clientX;
+        const cy = e.clientY;
+        // console.log(cx,cy);
+
+        const btntop = e.target.offsetTop;
+        const btnleft = e.target.offsetLeft;
+        // console.log(btntop,btnleft);
+
+        const xinside = cx - btnleft;
+        const yinside = cy - btntop;
+        // console.log(xinside,yinside);
+
+        //Create circle
+        const circle = document.createElement('span');
+        // circle.className = "circle";
+        circle.classList.add('circle');
+        // console.log(circle);
+
+        circle.style.top = yinside+'px';
+        circle.style.left = xinside+'px';
+
+        // e.target.appendChild(circle);
+        this.appendChild(circle);
+
+        // console.log(this);// သူကျတော့ details ပဲပြတာ
+        // console.log(e); //သူကကျတော့ information ကိုသွားတာ
+
+        setTimeout(()=>{
+            circle.remove();
+        },600);
     });
 });
+
+/*
+* this ဆိုတဲ့ function က regular function ပေါ်မှာပဲအလုပ်လုပ်တာ
+*
+* */
